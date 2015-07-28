c001edb49131ec6e5e4a697a63e9dd50d1fd0287
vindenergi <py@vindenergi.dk>
2015-07-28 16:37:28 +0200

    some small changes

--- MultipleActivitiesWithSameLayout/app/src/main/java/theme/multipleactivitieswithsamelayout/Activity2.java
+++ MultipleActivitiesWithSameLayout/app/src/main/java/theme/multipleactivitieswithsamelayout/Activity2.java
@@ -10,12 +10,22 @@ import android.widget.TextView;
 public class Activity2 extends ActionBarActivity {
 
     TextView text;
+	  Button btn;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
             text = (TextView) findViewById(R.id.textView);
+			 btn = (Button) findViewById(R.id.button);
             text.setText("Activity2");
+			 btn.setText("Back to Main Activity");
+			   btn.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+                     Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                     startActivity(intent);
+                 }
+             });
         }
     }
